9cb110bd2d2f3360cb41539512d1db73e2e24e09
elaf <elafghassangardi@gmail.com>
2024-02-17 14:25:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24
+let num4 = 5
+const mod = num3 % num4
+
+let set = [20, 4, 8, 0, 6, 7]
+const max = Math.max(...set)
